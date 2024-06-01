069665cc8a067da0321bb5f2a883ee71f0351bb7
alicesin615 <alicesin0615@gmail.com>
2024-06-01 19:32:19 +0800

    updated configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,72 @@
-import type { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
+import * as dotenv from "dotenv";
+import type { HardhatUserConfig } from "hardhat/types";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-deploy";
+// import 'hardhat-contract-sizer';
+// import '@appliedblockchain/chainlink-plugins-fund-link';
+
+dotenv.config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: "0.8.19",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      forking: {
+        url: SEPOLIA_RPC_URL !== undefined ? SEPOLIA_RPC_URL : "",
+        // blockNumber: Number(FORKING_BLOCK_NUMBER),
+        enabled: true,
+      },
+      saveDeployments: true,
+      tags: ["test", "local"],
+      loggingEnabled: true,
+      // accounts: {
+      // },
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL !== undefined ? SEPOLIA_RPC_URL : "",
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      chainId: 11155111,
+      saveDeployments: true,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 40000,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+  },
+
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer,
+    },
+    user: {
+      default: 1,
+    },
+  },
 };
 
 export default config;
