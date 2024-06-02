a4b247e1ebed5336f5eb6ac0a0442a61d73a9c60
alicesin615 <alicesin0615@gmail.com>
2024-06-03 07:57:37 +0800

    refactor minting & clean up code

--- contracts/LovePoem.sol
+++ contracts/LovePoem.sol
@@ -1,68 +1,159 @@
 //  SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.22;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "./LovePoemLib.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+
+enum RequestPurpose {
+	Mint,
+	UnlockSurprise
+}
+
+interface IVRFv2Consumer {
+	function prepRequest(RequestPurpose requestPurpose) external payable returns (uint256 requestId);
+
+	function getRequestStatus(uint256 requestId) external view returns (bool fulfilled, uint256[] memory randomWords, RequestPurpose requestPurpose);
+
+	function createNewSubscription() external returns (uint256 subscriptionId);
+
+	function topUpSubscription(uint256 amount) external;
+
+	function addConsumer(address consumerAddress) external;
+
+	function removeConsumer(address consumerAddress) external;
+
+	function cancelSubscription(address receivingWallet) external;
+
+	function withdraw(uint256 amount, address to) external;
+}
 
 contract LovePoem is ERC721URIStorage, Ownable {
-	using LovePoemLib for uint256;
+	IVRFv2Consumer private vrfConsumer;
+
+	uint256 public s_requestId;
+	bool public s_fulfilled;
+	uint256[] public s_randomWords;
+	RequestPurpose public s_requestPurpose;
 
 	string private baseURI;
+	uint256 public s_poemPrice;
+	uint256 public immutable maxSupply = 300;
+	uint256 private currentPoemId;
 
-	uint256 public immutable maxSupply = 30;
+	enum PhotoCard {
+		LILAC,
+		PALETTE,
+		BOOKMARK
+	}
+	enum ExclusiveAccess {
+		BEHIND_THE_SCENES,
+		UNRELEASED_TRACKS
+	}
 
-	mapping(uint256 => LovePoemLib.LovePoem) public poems;
-	mapping(LovePoemLib.PhotoCard => uint256) public ipfsHash;
+	struct LovePoemPlus {
+		PhotoCard photoCard;
+		ExclusiveAccess exclusiveAccess;
+		bool letterToIU;
+	}
 
-	uint256 private _currentPoemId;
+	mapping(uint256 => LovePoemPlus) public poems;
+	mapping(uint256 => string) private _tokenURIs;
 
+	event VRFConsumerRegistered();
+	event ReturnedRandomness(uint256 indexed requestId, uint256[] randomWords);
 	event PoemMinted(uint256 indexed poemId, address requester);
 	error FullyMinted();
 
-	constructor(string memory _initialBaseURI) ERC721("LovePoem", "POEM") Ownable() {
-		baseURI = _initialBaseURI;
+	constructor(string memory initialBaseURI) ERC721("LovePoem", "POEM") Ownable() {
+		baseURI = initialBaseURI;
+		currentPoemId = 1;
+	}
+
+	function registerVRFConsumer(address vrfConsumerAddress) external onlyOwner {
+		vrfConsumer = IVRFv2Consumer(vrfConsumerAddress);
+		emit VRFConsumerRegistered();
+	}
+
+	function requestRandomness(RequestPurpose _requestPurpose) external onlyOwner returns (uint256 requestId) {
+		s_requestPurpose = _requestPurpose;
+		s_requestId = vrfConsumer.prepRequest(_requestPurpose);
+		return s_requestId;
+	}
+
+	function receiveRandomness(uint256 _requestId) external onlyOwner returns (uint256[] memory) {
+		s_requestId = _requestId;
+		(bool fulfilled, uint256[] memory randomWords, RequestPurpose requestPurpose) = vrfConsumer.getRequestStatus(s_requestId);
+		s_fulfilled = fulfilled;
+		s_randomWords = randomWords;
+		s_requestPurpose = requestPurpose;
+		emit ReturnedRandomness(s_requestId, s_randomWords);
+		return s_randomWords;
 	}
 
 	function preparePoemToMint(address requester, uint256[] memory randomWords) external onlyOwner {
-		if (_currentPoemId >= maxSupply) {
+		if (currentPoemId == maxSupply) {
 			revert FullyMinted();
 		}
-		LovePoemLib.PhotoCard photoCard = getPhotoCard(randomWords[0]);
-		LovePoemLib.ExclusiveAccess exclusiveAccess = getExclusiveAccess(randomWords[1]);
-		LovePoemLib.LetterToIU letterToIU = getLetterToIU(randomWords[2]);
+
+		PhotoCard photoCard = PhotoCard(randomWords[0] % 3);
+		ExclusiveAccess exclusiveAccess = ExclusiveAccess(randomWords[1] % 2);
+		bool letterToIU = (randomWords[2] % 2) == 0;
+
 		mintPoem(requester, photoCard, exclusiveAccess, letterToIU);
 	}
 
-	function mintPoem(address to, LovePoemLib.PhotoCard _photoCard, LovePoemLib.ExclusiveAccess _exclusiveAccess, LovePoemLib.LetterToIU _letterToIU) internal onlyOwner {
-		_currentPoemId++;
-		poems[_currentPoemId] = LovePoemLib.LovePoem({photoCard: _photoCard, exclusiveAccess: _exclusiveAccess, letterToIU: _letterToIU});
+	function mintPoem(address to, PhotoCard _photoCard, ExclusiveAccess _exclusiveAccess, bool _letterToIU) internal onlyOwner {
+		LovePoemPlus memory lovePoemPlus = LovePoemPlus(_photoCard, _exclusiveAccess, _letterToIU);
+		poems[currentPoemId] = lovePoemPlus;
+		string memory assignedPhotoCard = _photoCard == PhotoCard.LILAC ? "1" : _photoCard == PhotoCard.PALETTE ? "2" : "3";
 
-		_safeMint(to, _currentPoemId);
-		_setTokenURI(_currentPoemId, string(abi.encodePacked(baseURI, _currentPoemId)));
-		emit PoemMinted(_currentPoemId, to);
+		_safeMint(to, currentPoemId);
+		_setTokenURI(currentPoemId, string(abi.encodePacked(baseURI, assignedPhotoCard, ".jpeg")));
+		emit PoemMinted(currentPoemId, to);
+		currentPoemId++;
+	}
+
+	/**
+	 * @notice Public poem info for verification
+	 */
+	function getPoemInfo(uint256 poemId) public view returns (LovePoemPlus memory poemInfo) {
+		return poems[poemId];
+	}
+
+	function getPhotoCard(uint256 poemId) private view onlyOwner returns (PhotoCard) {
+		return getPoemInfo(poemId).photoCard;
+	}
+
+	function getExclusiveAccess(uint256 poemId) private view onlyOwner returns (ExclusiveAccess) {
+		return getPoemInfo(poemId).exclusiveAccess;
+	}
+
+	function getLetterToIU(uint256 poemId) private view onlyOwner returns (bool) {
+		return getPoemInfo(poemId).letterToIU;
+	}
+
+	function currentCirculatingSupply() public view returns (uint256) {
+		return currentPoemId;
+	}
+
+	function getBaseURI() external view onlyOwner returns (string memory) {
+		return _baseURI();
 	}
 
 	function _baseURI() internal view virtual override onlyOwner returns (string memory) {
 		return baseURI;
 	}
 
-	function getPoemInfo(uint256 poemId) public view returns (LovePoemLib.LovePoem memory poemInfo) {
-		return poems[poemId];
+	function burnPoem(uint256 poemId) external onlyOwner {
+		_burn(poemId);
 	}
 
-	function getPhotoCard(uint256 poemId) private view onlyOwner returns (LovePoemLib.PhotoCard) {
-		return getPoemInfo(poemId).photoCard;
-	}
+	function _burn(uint256 poemId) internal virtual override {
+		super._burn(poemId);
 
-	function getExclusiveAccess(uint256 poemId) private view onlyOwner returns (LovePoemLib.ExclusiveAccess) {
-		return getPoemInfo(poemId).exclusiveAccess;
-	}
-
-	function getLetterToIU(uint256 poemId) private view onlyOwner returns (LovePoemLib.LetterToIU) {
-		return getPoemInfo(poemId).letterToIU;
-	}
-
-	function currentCirculatingSupply() public view returns (uint256) {
-		return _currentPoemId;
+		if (bytes(_tokenURIs[poemId]).length != 0) {
+			delete _tokenURIs[poemId];
+		}
 	}
 }
--- contracts/VRFv2Consumer.sol
+++ contracts/VRFv2Consumer.sol
@@ -1,36 +1,28 @@
 //  SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.22;
 
 import "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
 import "@chainlink/contracts/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
-import "./LovePoem.sol";
-import "./interfaces/ILovePoem.sol";
-import "hardhat/console.sol";
 
 contract VRFv2Consumer is ConfirmedOwner, VRFConsumerBaseV2 {
 	VRFCoordinatorV2Interface immutable COORDINATOR;
 	LinkTokenInterface immutable LINKTOKEN;
 
-	address vrfCoordinator = 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625;
-	address linkTokenContract = 0x779877A7B0D9E8603169DdbD7836e478b4624789;
-	bytes32 keyHash = 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c;
-	uint32 callbackGasLimit = 100000;
-	uint16 requestConfirmations = 3;
-	uint32 numWords = 3;
+	address vrfCoordinator;
+	address linkTokenContract;
+
+	uint64 private s_subscriptionId;
+	bytes32 s_keyHash;
+	uint32 s_callbackGasLimit;
+	uint16 s_requestConfirmations;
+	uint32 s_numWords;
+	address s_owner;
 
 	uint256[] public s_randomWords;
 	uint256 public s_requestId;
-	uint64 private s_subscriptionId;
-	address public s_owner;
-
-	// past requests Id.
-	uint256[] public requestIds;
-	uint256 public lastRequestId;
-
-	ILovePoem internal s_lovePoem;
-	uint256 public s_poemPrice;
+	RequestPurpose public s_requestPurpose;
 
 	enum RequestPurpose {
 		Mint,
@@ -41,65 +33,79 @@ contract VRFv2Consumer is ConfirmedOwner, VRFConsumerBaseV2 {
 		bool fulfilled;
 		bool exists;
 		RequestPurpose requestPurpose;
-		address requester;
 		uint256[] randomWords;
 	}
 
+	uint256[] public requestIds;
+	uint256 public lastRequestId;
 	mapping(uint256 => RequestStatus) public s_requests;
 
+	error RequestNotFound(uint256 requestId);
 	error InsufficientFunds(uint256 amount);
-	event RequestSent(uint256 requestId, uint32 numWords, RequestPurpose requestPurpose);
+	event RequestSent(uint256 requestId, RequestPurpose requestPurpose);
 	event RequestFulfilled(uint256 requestId, uint256[] randomWords, RequestPurpose requestPurpose);
 
-	constructor() VRFConsumerBaseV2(vrfCoordinator) ConfirmedOwner(msg.sender) {
-		COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
-		LINKTOKEN = LinkTokenInterface(linkTokenContract);
+	constructor(address _vrfCoordinator, address _linkTokenContract, bytes32 keyHash, uint16 requestConfirmations, uint32 callbackGasLimit, uint32 numWords) VRFConsumerBaseV2(_vrfCoordinator) ConfirmedOwner(msg.sender) {
+		COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator);
+		LINKTOKEN = LinkTokenInterface(_linkTokenContract);
+		s_keyHash = keyHash;
+		s_requestConfirmations = requestConfirmations;
+		s_callbackGasLimit = callbackGasLimit;
+		s_numWords = numWords;
 		s_owner = msg.sender;
-		//Create a new subscription when you deploy the contract.
-		_createNewSubscription();
+		createNewSubscription();
 	}
 
-	function mintNewPoem(uint256 currPoemPrice, RequestPurpose _requestPurpose) external payable returns (uint256 requestId) {
-		s_poemPrice = currPoemPrice;
-		if (msg.value < s_poemPrice) {
-			revert InsufficientFunds(msg.value);
-		}
-
-		return requestRandomWordsFor(_requestPurpose);
+	/**
+	 * @notice Prepares a request to be fulfilled
+	 * @param requestPurpose The purpose of the request to be defined by LovePoem : Enum RequestPurpose
+	 */
+	function prepRequest(RequestPurpose requestPurpose) external payable returns (uint256 requestId) {
+		s_requestPurpose = requestPurpose;
+		return requestRandomWordsFor(requestPurpose);
 	}
 
-	function requestRandomWordsFor(RequestPurpose _requestPurpose) internal returns (uint256 requestId) {
-		requestId = COORDINATOR.requestRandomWords(keyHash, s_subscriptionId, requestConfirmations, callbackGasLimit, numWords);
-		s_requests[requestId] = RequestStatus({fulfilled: false, exists: true, requestPurpose: _requestPurpose, requester: msg.sender, randomWords: new uint256[](0)});
+	function requestRandomWordsFor(RequestPurpose requestPurpose) internal returns (uint256 requestId) {
+		requestId = COORDINATOR.requestRandomWords(s_keyHash, s_subscriptionId, s_requestConfirmations, s_callbackGasLimit, s_numWords);
+		s_requests[requestId] = RequestStatus({fulfilled: false, exists: true, requestPurpose: requestPurpose, randomWords: new uint256[](0)});
 		requestIds.push(requestId);
 		lastRequestId = requestId;
-		emit RequestSent(requestId, numWords, _requestPurpose);
+		emit RequestSent(requestId, requestPurpose);
 		return requestId;
 	}
 
+	/**
+	 * @notice Fulfill a request processed by Chainlink VRF and returns the requested random words
+	 * as callback data
+	 */
 	function fulfillRandomWords(uint256 _requestId, uint256[] memory _randomWords) internal override {
-		require(s_requests[_requestId].exists, "request not found");
-		s_requests[_requestId].fulfilled = true;
-		s_requests[_requestId].randomWords = _randomWords;
+		if (s_requests[_requestId].exists == false) {
+			revert RequestNotFound(_requestId);
+		}
+		s_requestId = _requestId;
 		s_randomWords = _randomWords;
-		emit RequestFulfilled(_requestId, _randomWords, s_requests[_requestId].requestPurpose);
+		s_requestPurpose = s_requests[_requestId].requestPurpose;
+		s_requests[s_requestId].fulfilled = true;
+		s_requests[s_requestId].randomWords = s_randomWords;
+		emit RequestFulfilled(s_requestId, s_randomWords, s_requestPurpose);
 	}
 
-	function getRequestStatus(uint256 _requestId) external view returns (bool fulfilled, uint256[] memory randomWords) {
-		require(s_requests[_requestId].exists, "request not found");
-		RequestStatus memory request = s_requests[_requestId];
-		return (request.fulfilled, request.randomWords);
+	function getRequestStatus(uint256 requestId) external view returns (bool fulfilled, uint256[] memory randomWords, RequestPurpose requestPurpose) {
+		if (s_requests[requestId].exists == false) {
+			revert RequestNotFound(requestId);
+		}
+		RequestStatus memory requestStatus = s_requests[requestId];
+		return (requestStatus.fulfilled, requestStatus.randomWords, requestStatus.requestPurpose);
 	}
 
 	// Create a new subscription when the contract is initially deployed.
-	function _createNewSubscription() internal onlyOwner {
+	function createNewSubscription() internal onlyOwner returns (uint256 subscriptionId) {
 		s_subscriptionId = COORDINATOR.createSubscription();
 		// Add this contract as a consumer of its own subscription.
 		COORDINATOR.addConsumer(s_subscriptionId, address(this));
+		return s_subscriptionId;
 	}
 
-	// Assumes this contract owns link.
-	// 1000000000000000000 = 1 LINK
 	function topUpSubscription(uint256 amount) external onlyOwner {
 		LINKTOKEN.transferAndCall(address(COORDINATOR), amount, abi.encode(s_subscriptionId));
 	}
@@ -120,7 +126,6 @@ contract VRFv2Consumer is ConfirmedOwner, VRFConsumerBaseV2 {
 		s_subscriptionId = 0;
 	}
 
-	// 1000000000000000000 = 1 LINK
 	function withdraw(uint256 amount, address to) external onlyOwner {
 		LINKTOKEN.transfer(to, amount);
 	}
