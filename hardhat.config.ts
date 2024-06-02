5d575494b024577a42ebb3727a893de9569193c0
alicesin615 <alicesin0615@gmail.com>
2024-06-03 07:18:56 +0800

    updated configs & abis

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,8 +4,6 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomiclabs/hardhat-ethers";
 import "hardhat-deploy";
-// import 'hardhat-contract-sizer';
-// import '@appliedblockchain/chainlink-plugins-fund-link';
 
 dotenv.config();
 
@@ -14,7 +12,7 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.22",
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
