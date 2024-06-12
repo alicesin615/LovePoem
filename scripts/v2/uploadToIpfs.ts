23a2b7d8746ceedc5137e0dc87e9a4ff777a7f35
alicesin615 <alicesin0615@gmail.com>
2024-06-13 02:10:50 +0800

    update prep metadata script to verify if local image already uploaded to ipfs

--- scripts/v2/uploadToIpfs.ts
+++ scripts/v2/uploadToIpfs.ts
@@ -3,9 +3,15 @@ import path from "path";
 import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import { create } from "@web3-storage/w3up-client";
-import { AnyLink, FileLike } from "@web3-storage/w3up-client/types";
+import {
+  AnyLink,
+  Client,
+  FileLike,
+  UnknownLink,
+  UploadListSuccess,
+} from "@web3-storage/w3up-client/types";
 import { filesFromPaths } from "files-from-path";
-import { Account } from "@web3-storage/w3up-client/account";
+import { Metadata } from "../../models/common.model";
 dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
@@ -25,26 +31,71 @@ async function getFilesFromPath(path: string) {
  * @param {FileLike[]} images - Array of images to upload
  * @return {AnyLink} The CID of the uploaded directory
  */
-async function uploadToIpfs(images: FileLike[]) {
-  const client = await create();
-  let account: Account | undefined = undefined;
-  try {
-    account = await client.login(WEB3_ACCOUNT);
-  } catch (error) {
-    console.error("Error loging in: ", error.message);
-  }
-
-  await client.setCurrentSpace(`did:key:${WEB3_SPACE_KEY}`);
-
-  let directoryCid: AnyLink | undefined = undefined;
+async function uploadToIpfs(
+  client: Client,
+  images: FileLike[],
+): Promise<AnyLink> {
+  let directoryCid: UnknownLink;
   try {
     directoryCid = await client.uploadDirectory(images);
   } catch (error) {
     console.error("Error uploading images: ", error.message);
   }
 
-  console.log("directoryCid: ", directoryCid);
   return directoryCid;
+}
+
+/**
+ * Verifies if an image with the given CID has been successfully uploaded to IPFS using the provided js client.
+ *
+ * @param {Client} client - The js client instance to interact with the w3up platform
+ * @param {string} cid - The CID (Content Identifier) of the image to verify against {UploadListSuccess}.
+ * @return {Promise<boolean>} A boolean indicating whether the image has been successfully uploaded or not.
+ */
+async function verifyImageOnIpfs(
+  client: Client,
+  cid: string,
+): Promise<boolean> {
+  let verified = false;
+  let uploadedList: UploadListSuccess | undefined = undefined;
+
+  try {
+    uploadedList = await client.capability.upload.list();
+  } catch (error) {
+    console.error("Error verifying image on IPFS: ", error.message);
+  }
+
+  if (uploadedList?.size) {
+    verified = uploadedList?.before === cid;
+  }
+
+  return verified;
+}
+
+/**
+ * Sets up the client by creating a new client, logging in with the web3 account,
+ * and setting the current space to the specified Space DID {WEB3_SPACE_KEY}.
+ *
+ * @return {Promise<Client>} The client that has been set up locally with the web3 storage account.
+ */
+async function setUpClient(): Promise<Client> {
+  const client = await create();
+  try {
+    await client.login(WEB3_ACCOUNT);
+  } catch (error) {
+    console.error("Error loging in: ", error.message);
+  }
+
+  try {
+    await client.setCurrentSpace(`did:key:${WEB3_SPACE_KEY}`);
+  } catch (error) {
+    console.error(
+      "Error setting up web3 storage client in local space: ",
+      error.message,
+    );
+  }
+
+  return client;
 }
 
 /**
@@ -59,11 +110,10 @@ async function parseMetadataFile(
   id: number,
   metadataDirPath: string,
   imagesDirPath: string,
-): Promise<object> {
+): Promise<Metadata> {
+  const client = await setUpClient();
   const imagesPath = path.join(imagesDirPath, `${id}.jpeg`);
   const imageFiles = await getFilesFromPath(imagesPath);
-  const imageCID = await uploadToIpfs(imageFiles);
-  console.log("imageCID: ", imageCID);
 
   let metadataFile: Buffer = Buffer.from("");
   const metadataFilePath = path.join(metadataDirPath, `${id}`);
@@ -72,8 +122,26 @@ async function parseMetadataFile(
   } catch (error) {
     console.error(`Error reading file in metadata directory: ${error.message}`);
   }
+
   const metadataJson = JSON.parse(metadataFile.toString());
-  metadataJson.image = `https://${imageCID}.ipfs.w3s.link/`;
+
+  let verified = false;
+  if (metadataJson.image) {
+    const cid: string = metadataJson.image.match(
+      /https:\/\/([a-zA-Z0-9]+)\.ipfs\.w3s\.link/,
+    )?.[1];
+    try {
+      verified = await verifyImageOnIpfs(client, cid);
+      console.log(`Verified image ${id} already uploaded on IPFS: ${verified}`);
+    } catch (error) {
+      console.error(`Error verifying image ${id} on IPFS:`, error.message);
+    }
+  }
+
+  if (!verified) {
+    const imageCID = await uploadToIpfs(client, imageFiles);
+    metadataJson.image = `https://${imageCID}.ipfs.w3s.link/`;
+  }
 
   const metadataFileBuffer = Buffer.from(JSON.stringify(metadataJson));
   try {
@@ -81,7 +149,6 @@ async function parseMetadataFile(
   } catch (error) {
     console.error(`Error writing file in metadata directory: ${error.message}`);
   }
-  console.log("metadataJson: ", metadataJson);
   return metadataJson;
 }
 
@@ -89,10 +156,10 @@ async function parseMetadataFile(
  * Prepares the metadata by parsing each metadata file in the specified directory,
  * updating the image field with the IPFS CID, and adding a new field called `id` to each metadata object.
  * @example metadata/0 (omit the extension) | images/0.jpeg
- * @return {Promise<object[]>} An array of parsed metadata objects with the `id` field added.
+ * @return {Promise<Metadata[]>} An array of parsed metadata objects with the `id` field added.
  */
-async function prepareMetadata() {
-  const finalMetadata: object[] = [];
+export async function prepareMetadata(): Promise<Metadata[]> {
+  const finalMetadata: Metadata[] = [];
   const metadataDirPath = path.join(__dirname, "../../", "metadata");
   const imagesDirPath = path.join(__dirname, "../../", "images");
 
@@ -115,9 +182,3 @@ async function prepareMetadata() {
   console.log("finalMetadata: ", finalMetadata);
   return finalMetadata;
 }
-prepareMetadata()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
