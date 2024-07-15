8d4fe5783501c16f8d1fe663c3349b1f7167cb4b
alicesin615 <alicesin0615@gmail.com>
2024-07-16 07:37:30 +0800

    add test cases for updating metadata

--- test/v2/LovePoemV2.spec.ts
+++ test/v2/LovePoemV2.spec.ts
@@ -2,21 +2,26 @@ import hre, { network } from "hardhat";
 import { expect } from "chai";
 import { describe, it } from "mocha";
 import { getAccounts, getDatabase } from "@tableland/local";
-import type { Signer } from "@tableland/sdk/helpers";
-import { getDefaultProvider } from "@tableland/sdk/helpers";
 import { networkConfig } from "../../helper-hardhat-config";
 import { LovePoemV2, LovePoemV2__factory } from "../../typechain-types";
 import { LovePoemV2Database } from "../../models";
-import { createLovePoemTables, prepareSqlForTables } from "../../scripts/v2";
-
+import {
+  createTables,
+  getJoinedTable,
+  prepareSqlForTables,
+} from "../../scripts/v2";
+import { retrieveMetadata } from "../../utils/retrieve-abi";
+import {
+  selectFromJoinedTable,
+  updateAttributes,
+} from "../../scripts/v2/prepareTables";
 describe("LovePoemV2 Unit Tests", async function () {
   // Set a timeout for spinning up the Local Tableland instance during setup
   this.timeout(20000);
 
   let LovePoemV2Factory: LovePoemV2__factory;
   let lovePoemV2: LovePoemV2;
-  let owner: Signer;
-  let wallet: Signer;
+
   let db: LovePoemV2Database;
   let mainLovePoemTableName: string;
   let attributesLovePoemTableName: string;
@@ -30,8 +35,7 @@ describe("LovePoemV2 Unit Tests", async function () {
   const mainTableSchema = `id int primary key, name text, description text, image text`;
   const attributesTableSchema = `main_id int not null, trait_type text not null, value text`;
   // const tablelandBaseURI = helpers.getBaseUrl(chainId);
-
-  const provider = getDefaultProvider(tablelandBaseURI);
+  // const provider = getDefaultProvider(tablelandBaseURI);
   db = getDatabase(accounts?.[1]);
 
   before(async () => {
@@ -54,7 +58,7 @@ describe("LovePoemV2 Unit Tests", async function () {
   });
 
   it("Should create LovePoemV2 main table", async function () {
-    mainLovePoemTableName = await createLovePoemTables(
+    mainLovePoemTableName = await createTables(
       mainTablePrefix,
       mainTableSchema,
       db,
@@ -63,7 +67,7 @@ describe("LovePoemV2 Unit Tests", async function () {
     expect(mainLovePoemTableName).to.equal(`${mainTablePrefix}_${chainId}_2`);
   });
   it("Should create LovePoemV2 attributes table", async function () {
-    attributesLovePoemTableName = await createLovePoemTables(
+    attributesLovePoemTableName = await createTables(
       attributesTablePrefix,
       attributesTableSchema,
       db,
@@ -72,7 +76,7 @@ describe("LovePoemV2 Unit Tests", async function () {
       `${attributesTablePrefix}_${chainId}_3`,
     );
   });
-  it("Should join LovePoemV2 tables and insert metadata", async function () {
+  it("Should join LovePoemV2 tables and insert original metadata", async function () {
     // Prep sql statements to join two tables & insert metadata
     const sqlInsertStatements = await prepareSqlForTables(
       mainLovePoemTableName,
@@ -91,7 +95,7 @@ describe("LovePoemV2 Unit Tests", async function () {
           insertIntoMainResult?.error,
         );
       }
-      // Insert attributes
+      // Insert attributes to attributes table corresponding to each main table row (id = main.id)
       for await (let attribute of attributes) {
         const { meta: insertIntoAttributes } = await db
           .prepare(attribute)
@@ -114,7 +118,7 @@ describe("LovePoemV2 Unit Tests", async function () {
       expect(insertIntoMainResult?.tableId).to.equal("2");
     }
   });
-  it("Should get the initial baseURI of LovePoemV2", async function () {
+  it("Should get initial baseURI of LovePoemV2 same as tableland baseURI of given network", async function () {
     const lovePoemV2BaseURI = await lovePoemV2.getBaseURIString();
     expect(lovePoemV2BaseURI).to.equal(tablelandBaseURI);
   });
@@ -130,5 +134,39 @@ describe("LovePoemV2 Unit Tests", async function () {
     const tokenURI = await lovePoemV2.tokenURI(tokenId);
     console.log(`'tokenURI' using token '${tokenId}' here:\n${tokenURI}`);
     expect(tokenId.toNumber()).to.equal(0);
+  });
+  it("Should return the joined LovePoemV2 table in JSON format equal to the original metaddata", async function () {
+    const [joinedTable] = await getJoinedTable(
+      db,
+      mainLovePoemTableName,
+      attributesLovePoemTableName,
+    );
+    const metadata = JSON.parse(JSON.stringify(joinedTable?.metadata));
+    const retrievedOriginalMetadata = JSON.parse(retrieveMetadata("0")!!);
+
+    expect(metadata).to.deep.equal(retrievedOriginalMetadata);
+  });
+  it("Should update the status attribute of LovePoemV2 to GATE_OPEN", async function () {
+    await updateAttributes(
+      db,
+      attributesLovePoemTableName,
+      "Status",
+      "GATE_OPEN",
+    );
+
+    const selectAttributeStmt = `json_group_array(json_object('trait_type',trait_type,'value', value)) as attributes `;
+    const [updatedAttributesObj] = await selectFromJoinedTable(
+      db,
+      mainLovePoemTableName,
+      attributesLovePoemTableName,
+      selectAttributeStmt,
+    );
+    const updatedAttribute = (
+      updatedAttributesObj?.attributes as unknown as {
+        trait_type: string;
+        value: string;
+      }[]
+    )?.find(({ trait_type }) => trait_type === "Status");
+    expect(updatedAttribute?.value).to.equal("GATE_OPEN");
   });
 });
