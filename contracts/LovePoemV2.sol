d4104dbb0e3ffbf222e76f2f37809f198bdee74d
alicesin615 <alicesin0615@gmail.com>
2024-07-23 00:52:51 +0800

    update contract to include validation checks

--- contracts/LovePoemV2.sol
+++ contracts/LovePoemV2.sol
@@ -5,19 +5,25 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "./LovePoemLib.sol";
 
 /**
- * @dev Implementation of a simple NFT, using Tableland to host metadata in a two table setup.
+ * @dev Implementation of an Event Ticketing NFT, using Tableland to host metadata in a two table setup.
  */
 contract LovePoemV2 is ERC721URIStorage, Ownable {
 	string private baseURI;
-	// Schema: id int primary key, name text, description text, image text
 	string private mainTable;
-	// Schema: main_id int not null, trait_type text not null, value text
 	string private attributesTable;
 
-	uint256 public immutable maxSupply = 5;
+	uint256 public immutable maxSupply = 300;
 	uint256 private tokenIdCounter;
+
+	mapping(uint256 => address) public tokenHolders; // mapping(tokenId => holderAddress)
+
+	event HoldershipTransferred(uint256 tokenId, address oldHolder, address newHolder);
+
+	error InvalidHolder(uint256 tokenId, address requester);
+	error InvalidTokenId(uint256 tokenId);
 
 	constructor(
 		string memory _initialBaseURI,
@@ -25,10 +31,23 @@ contract LovePoemV2 is ERC721URIStorage, Ownable {
 		string memory _attributesTable
 	) ERC721("LovePoem", "POEM") onlyOwner() {
 		tokenIdCounter = 0;
-		maxSupply = 5;
 		baseURI = _initialBaseURI;
 		mainTable = _mainTable;
 		attributesTable = _attributesTable;
+	}
+
+	modifier onlyHolder(uint256 tokenId) {
+		if (tokenHolders[tokenId] != msg.sender) {
+			revert InvalidHolder(tokenId, msg.sender);
+		}
+		_;
+	}
+
+	modifier validToken(uint256 tokenId) {
+		if (_exists(tokenId) == false) {
+			revert InvalidTokenId(tokenId);
+		}
+		_;
 	}
 
 	function _baseURI() internal view virtual override returns (string memory) {
@@ -46,34 +65,14 @@ contract LovePoemV2 is ERC721URIStorage, Ownable {
 	 *  @dev Must override the default implementation, which simply appends a `tokenId` to _baseURI.
 	 *  @param tokenId - The id of the NFT token that is being requested
 	 */
-	function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-		require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+	function tokenURI(uint256 tokenId) public view virtual override validToken(tokenId) returns (string memory) {
 		string memory baseURIString = _baseURI();
 
-		if (bytes(baseURIString).length == 0) {
+		if (bytes(baseURIString).length == 0 || tokenHolders[tokenId] != msg.sender) {
 			return "";
 		}
 
-		/**
-		 *   An SQL query to JOIN two tables to compose the metadata accross a 'main' and 'attributes' table
-		 *
-		 *   SELECT json_object(
-		 *       'id', id,
-		 *       'name', name,
-		 *       'description', description,
-		 *       'image', image,
-		 *       'attributes', json_group_array(
-		 *           json_object(
-		 *               'trait_type',trait_type,
-		 *               'value', value
-		 *           )
-		 *       )
-		 *   )
-		 *   FROM {mainTable} JOIN {attributesTable}
-		 *       ON {mainTable}.id = {attributesTable}.main_id
-		 *   WHERE id = <main_id>
-		 *   GROUP BY id
-		 */
+		// An SQL query to JOIN two tables to compose the metadata accross a 'main' and 'attributes' table
 		string memory query = string(
 			abi.encodePacked(
 				"SELECT%20json_object%28%27id%27%2Cid%2C%27name%27%2Cname%2C%27description%27%2Cdescription%2C%27image%27%2Cimage%2C%27attributes%27%2Cjson_group_array%28json_object%28%27trait_type%27%2Ctrait_type%2C%27value%27%2Cvalue%29%29%29%20FROM%20",
@@ -87,6 +86,7 @@ contract LovePoemV2 is ERC721URIStorage, Ownable {
 				"%2Emain_id%20WHERE%20id%3D"
 			)
 		);
+
 		// Return the baseURI with a query string, which looks up the token id in a row.
 		return
 			string(
@@ -105,8 +105,26 @@ contract LovePoemV2 is ERC721URIStorage, Ownable {
 	 */
 	function mint() public {
 		require(tokenIdCounter < maxSupply, "Maximum number of tokens have been minted");
-		_safeMint(msg.sender, tokenIdCounter);
 		tokenIdCounter++;
+		_safeMint(msg.sender, tokenIdCounter);
+	}
+
+	/**
+	 * @notice Fn to be used to verify if caller is the token holder before
+	 * allowing caller to make changes to the token holdership or its metadata
+	 **/
+	function isHolder(uint256 tokenId) public view returns (bool) {
+		if (tokenHolders[tokenId] != msg.sender) {
+			revert InvalidHolder(tokenId, msg.sender);
+		}
+		return _ownerOf(tokenId) == msg.sender;
+	}
+
+	function transferHoldership(address newHolder, uint256 tokenId) external onlyHolder(tokenId) {
+		if (isHolder(tokenId)) {
+			tokenHolders[tokenId] = newHolder;
+			emit HoldershipTransferred(tokenId, msg.sender, newHolder);
+		}
 	}
 
 	function totalSupply() public pure returns (uint256) {
